362f6a1739c5d5b90b90a8c52b79f06c38e18304
Pranav132 <mais.06pranav@gmail.com>
2023-01-03 22:19:05 +0530

    header change

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -42,7 +42,7 @@ function HeroHome() {
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">The <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 whitespace-nowrap">&lt;Coding /&gt;</span> Company</h1>
             <br></br><br></br>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">At The Coding Company, we turn your ideas into reality through expert coding services. Our team of experienced developers specialize in bringing your unique vision to life, creating custom software solutions to meet the needs of your business or project</p>
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">We turn your ideas into reality through expert coding services. Our team of experienced developers specialize in bringing your unique vision to life, creating custom software solutions to meet the needs of your business or project, and that's not all we do.</p>
               <br></br>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
