e049a8b852f8077570343e415eb183bbb0536a81
Pranav132 <mais.06pranav@gmail.com>
2023-01-04 01:23:34 +0530

    minor responsiveness fix in contact section

--- src/partials/Newsletter.jsx
+++ src/partials/Newsletter.jsx
@@ -45,7 +45,7 @@ function Newsletter() {
                 <p className="text-gray-300 text-lg mb-6">Thank you for considering The Coding Company for your project needs. Please don't hesitate to contact us to schedule a consultation and discuss how we can bring your vision to life.</p>
 
                 {/* CTA form */}
-                  <div className="flex text-white">
+                  <div className="flex text-white justify-center lg:justify-start">
                     <a href="mailto:contact@codingcompany.dev" target="_blank" className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0" id="open-3">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="16" width="16"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
                     </a>
